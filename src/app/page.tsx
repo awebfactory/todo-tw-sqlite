a9fd892603bcda306476788721a81c3f4d707eef
victorkane <victorkane@gmail.com>
2023-09-29 15:28:07 -0300

    build: scaffold server component data fetching and creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
+import { prisma } from "@/db"
 import Link from "next/link"
 
-export default function Home() {
+export default async function Home() {
+  const todos = await prisma.todo.findMany()
+  await prisma.todo.create({ data: { title: "test", complete: false } })
   return (
     <>
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">Todos</h1>
         <Link
-          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-  within:bg-slate-700 outline-none"
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
           href="/new"
         >
           New
         </Link>
       </header>
+      <ul className="p1-4">
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </>
   )
 }
